d1b9c9545256c88e0169af954f4cb5a151978333
Eneko Rivera de León <enekorivera@MacBook-Air-de-Eneko.local>
2026-02-17 23:57:56 +0100

    Add test-messages page with fullwidth layout and revert messages page

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ import NotFound from "./pages/NotFound";
 // Lazy loaded - non-critical pages
 const Bookings = lazy(() => import("./pages/Bookings"));
 const Messages = lazy(() => import("./pages/Messages"));
+const TestMessages = lazy(() => import("./pages/TestMessages"));
 const Applications = lazy(() => import("./pages/landlord/Applications"));
 const Landlords = lazy(() => import("./pages/Landlords"));
 const HowItWorks = lazy(() => import("./pages/HowItWorks"));
@@ -190,6 +191,9 @@ const App = () => (
                                 } />
                                 <Route path="/messages/:threadId" element={
                                   <Messages />
+                                } />
+                                <Route path="/test-messages" element={
+                                  <TestMessages />
                                 } />
 
                                 {/* Protected Routes - User */}
